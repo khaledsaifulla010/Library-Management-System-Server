4a76ee99d2ea2a9ae44a0ed057313c763b92112e
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-08-27 22:22:52 +0600

    fix cors issue

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 import express, { Application, Request, Response } from "express";
+import cors from "cors"; 
 import { bookRoutes } from "./app/controllers/book.controller";
 import { borrowRoutes } from "./app/controllers/borrow.controller";
 
 const app: Application = express();
+
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 
 // CONTROLLERS //
